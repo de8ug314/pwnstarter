128846a1cc7f3632a87c91300a99aab2cd2880dd
de8ug <1873121488@qq.com>
2022-02-23 13:47:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod 777 ./pwnstarter.sh
-sed -e 1a\this_path=\'`pwd`\' -i pwnstarter.sh
+sed -e 7a\this_path=\'`pwd`\' -i pwnstarter.sh
 ln ./pwnstarter.sh /usr/bin/pwnstarter
 if [ -f "uninstall.sh" ]
 then
